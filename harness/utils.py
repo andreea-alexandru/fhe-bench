2a9adebf1417556646c012427df53d9fc88f5188
Shai Halevi <shaihal@amazon.com>
2025-08-14 19:19:09 -0400

    README and pylint

--- harness/utils.py
+++ harness/utils.py
@@ -16,8 +16,8 @@ import argparse
 import json
 from datetime import datetime
 from pathlib import Path
-from params import InstanceParams, TOY, LARGE
 from typing import Tuple
+from params import InstanceParams, TOY, LARGE
 
 # Global variable to track the last timestamp
 _last_timestamp: datetime = None
@@ -54,15 +54,15 @@ def parse_submission_arguments(workload: str) -> Tuple[int, InstanceParams, int,
 
 def ensure_directories(rootdir: Path):
     """ Check that the current directory has sub-directories
-    'harness', 'scripts', and 'submission' """
-    required_dirs = ['harness', 'scripts', 'submission']
+    'harness' and 'submission' """
+    required_dirs = ['harness', 'submission']
     for dir_name in required_dirs:
         if not (rootdir / dir_name).exists():
             print(f"Error: Required directory '{dir_name}'",
                   f"not found in {rootdir}")
             sys.exit(1)
 
-def build_submission(script_dir: Path):
+def build_submission(_script_dir: Path):
     """
     Build the submission
     """
@@ -88,12 +88,15 @@ def log_step(step_num: int, step_name: str, start: bool = False):
     # Update the last timestamp for the next call
     _last_timestamp = now
 
-    if (not start):
+    if not start:
         print(f"{timestamp} [harness] {step_num}: {step_name} completed{elapsed_str}")
         _timestampsStr[step_name] = f"{round(elapsed_seconds, 4)}s"
         _timestamps[step_name] = elapsed_seconds
 
 def log_size(path: Path, object_name: str, flag: bool = False, previous: int = 0):
+    """
+    Helper function to print timestamp after each step with second precision
+    """
     global _bandwidth
 
     if not path.exists():
@@ -101,15 +104,16 @@ def log_size(path: Path, object_name: str, flag: bool = False, previous: int = 0
     else:
         size = int(subprocess.run(["du", "-sb", path], check=True,
                            capture_output=True, text=True).stdout.split()[0])
-        if(flag):
+        if flag:
             size -= previous
-    
+
     print("         [harness]", object_name, "size:", human_readable_size(size))
 
     _bandwidth[object_name] = human_readable_size(size)
     return size
 
 def human_readable_size(n: int):
+    """Pretty print for size in bytes"""
     for unit in ["B","K","M","G","T"]:
         if n < 1024:
             return f"{n:.1f}{unit}"
@@ -117,6 +121,7 @@ def human_readable_size(n: int):
     return f"{n:.1f}P"
 
 def save_run(path: Path):
+    """Save the timing from the current run to disk"""
     global _timestamps
     global _timestampsStr
     global _bandwidth
